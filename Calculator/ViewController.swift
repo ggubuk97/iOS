5a210e0d1fd4454bb5116f1059ddc0d81eb00ad6
Jaden Jung <jaden@Jadenui-MacBook-Pro.local>
2017-10-25 17:40:49 +0900

    Initial commit for Calculator

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jaden Jung on 2017. 10. 25..
+//  Copyright © 2017년 Jaden Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+}
+
