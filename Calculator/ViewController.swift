be851b0294aa04a5b567199af8eb7c661f3e7ec1
Jaden Jung <jaden@10.nate.com>
2017-10-26 17:04:25 +0900

    Complete Lecture 1.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        print("\(digit) was called")
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }
+        else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
 
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                //display.text = String(Double.pi)
+                displayValue = Double.pi
+            case "√":
+                //let operand = Double(display.text!)!
+                //display.text = String(sqrt(operand))
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
     
 }
 
